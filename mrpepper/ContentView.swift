527f1a5943243a2456a220086c448da71c9a5557
Jakub <jakub.berisha@bekacom.at>
2024-09-16 12:23:30 +0200

    Initial Commit

---
+++ mrpepper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mrpepper
+//
+//  Created by Jakub Berisha on 16.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mrpepper/mrpepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  mrpepperApp.swift
+//  mrpepper
+//
+//  Created by Jakub Berisha on 16.09.24.
+//
+
+import SwiftUI
+
+@main
+struct mrpepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
